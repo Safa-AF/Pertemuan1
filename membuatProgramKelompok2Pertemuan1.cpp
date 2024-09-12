79eb2f169c23aace29551bf49ceb5c49b35d3971
Safa-AF <fadhilsafarazakma@gmail.com>
2024-09-12 12:29:31 +0700

    Add files via upload

--- membuatProgramKelompok2Pertemuan1.cpp
+++ membuatProgramKelompok2Pertemuan1.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 int main() {
 
-    int i, n, t = 0;
+    int i, n, a, t = 0;
 
     cout << "masukkan banyak nilai: ";
     cin >> n;
-    n *= 2;
-
     cout << endl;
 
-    for (i = 2; i <= n; i += 2) {
-            cout << i << " ";
-            t += i;
+    for (i = 1; i <= n; i++) {
+            cout << "masukkan nilai ke-"<< i << " : ";\
+            cin >> a;
+            t += a;
     }
 
-    cout << "\nJumlah : " << t << endl;
+    cout << "\nJumlahnya adalah : " << t << endl;
     return 0;
 }
